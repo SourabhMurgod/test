6e0eb6caa83624d15e090ba92bf4d4d1ec8d8266
SourabhMurgod <murgodsourabh@gmail.com>
2024-02-25 11:08:25 +0530

    new push for pipeline21

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -20,7 +20,7 @@ const Register = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      await axios.post("http://3.136.187.122:8800/api/auth/register", inputs);
+      await axios.post("https://api-dev-react-cluster.vishalaws.tech/api/auth/register", inputs);
       navigate("/login");
     } catch (err) {
       setError(err.response.data);
